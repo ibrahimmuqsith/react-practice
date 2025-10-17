a9025579046060a8d17b7727a00af1ba74add134
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-17 12:35:34 +0530

    methods of calling a comp

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // const jsxHeading  = <h1 key={'h1'}> Hello World from JSX </h1>
+const jsContent = "this is a jsContent"
 
 const Heading = () => <h1> Hello World from React HeadingComponent </h1>
 
 const HeadingComponent = () => {
     return (
         <div className='container'>
+            {Heading()}
             <Heading />
+            <Heading></Heading>
+            <h2 className='jsContent'>
+                {jsContent}
+            </h2>
         </div>
     )
 }
