3a69646275a2cc269eb8b81bf22041de31870f7f
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-17 14:01:20 +0530

    created dummy layout of app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+    return (
+        <div className='app'>
+            {/* <Header /> */}
+            {/* <Body /> */}
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
