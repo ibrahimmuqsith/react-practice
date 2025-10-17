7802e8da7352c3107d7d04ea1445f68208af2b15
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-17 12:12:16 +0530

    comp composition = comp inside another comp

--- App.js
+++ App.js
@@ -1,36 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement(
-    'h1',
-    {
-        // this adds attributes to the HTML tag
-        id: "heading",
-        testing: "muqsith"
-    },
-    "Hello World from React !!"
-)
+// const jsxHeading  = <h1 key={'h1'}> Hello World from JSX </h1>
 
-const parent = React.createElement('div', { id: 'container' },
-    [
-        React.createElement('div', { id: 'parent1', key: 'parent1' },
-            [
-                React.createElement('h1', { id: 'heading1', key: 'heading1' }, "this is parent1 - H1 tag"),
-                React.createElement('h2', { id: 'heading2', key: 'heading2' }, "this is parent1 - H2 tag"),
-            ]
-        ),
-        React.createElement('div', { id: 'parent2', key: 'parent2' },
-            [
-                React.createElement('h3', { id: 'heading3', key: 'heading3' }, "this is parent2 - H3 tag"),
-                React.createElement('h4', { id: 'heading4', key: 'heading4' }, "this is parent2 - H4 tag"),
-            ]
-        )
-    ]
-)
+const Heading = () => <h1> Hello World from React HeadingComponent </h1>
 
-console.log(parent)
-//returns a react h1 element ==> JS object with a lot of details
+const HeadingComponent = () => {
+    return (
+        <div className='container'>
+            <Heading />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading)
-root.render(parent)
+
+// root.render(jsxHeading)
+root.render(<HeadingComponent />)
