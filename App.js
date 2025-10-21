c7abd2fe41c84b0037858a579b853a2278176e58
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-21 06:52:13 +0530

    added header

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logoContainer'>
+                <img
+                    alt='logo'
+                    className='logo'
+                    src='https://static.vecteezy.com/system/resources/previews/011/405/724/non_2x/call-food-logo-design-food-delivery-service-logo-concept-free-vector.jpg'
+                />
+            </div>
+            <div className='navBar'>
+                <ul>
+                    <li> Home </li>
+                    <li> About </li>
+                    <li> Profile </li>
+                    <li> Cart </li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <div className='app'>
-            {/* <Header /> */}
+            <Header />
             {/* <Body /> */}
             {/* <Footer /> */}
         </div>
