16d129b3dd0335a3195a1305e55b86c965a61738
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-24 21:12:31 +0530

    added CDN_CORS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+export const CDN_CORS = 'https://corsproxy.io/?'
+
 export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
 export const CDN_LOGO = 'https://static.vecteezy.com/system/resources/previews/011/405/724/non_2x/call-food-logo-design-food-delivery-service-logo-concept-free-vector.jpg';
