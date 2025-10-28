c85c583681592e05e9870bb30ecee9976fd17d94
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-28 10:35:58 +0530

    menu container with styles

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react"
-import { MENU_ENDPOINT } from "../utils/constants"
-import { MENU_DATA } from "../utils/mockData"
+import {
+    CDN_URL,
+    MENU_ENDPOINT
+} from "../utils/constants"
+import {
+    MENU_DATA,
+    SINGLE_RESTAURANT_DATA
+} from "../utils/mockData"
+import Shimmer from "./Shimmer"
 
 const Menu = () => {
     const [menu, setMenu] = useState(null)
+    const [restaurantInfo, setRestaurantInfo] = useState(null)
 
     useEffect(() => {
         fetchMenu()
@@ -15,15 +23,54 @@ const Menu = () => {
             // const data = await res.json();
             // console.log("data", data)
             setMenu(MENU_DATA)
+            setRestaurantInfo(SINGLE_RESTAURANT_DATA)
         } catch (err) {
             console.log(err)
         }
     }
 
+    if (!menu || !restaurantInfo) {
+        return <Shimmer
+            cards={8}
+            width={320}
+            height={300}
+        />
+    }
+
     return (
         <div className="menucontainer">
-            <h1>Hotel</h1>
+            <div className="restaurantInfo">
+                <h1> {restaurantInfo?.name} </h1>
+                <div className="flex-space-between">
+                    <p> {restaurantInfo?.costForTwo} </p>
+                    <p> {restaurantInfo?.avgRating} stars </p>
+                </div>
+                <div className="flex-space-between">
+                    <p> {restaurantInfo?.cuisines.join(', ')} </p>
+                    <p> {restaurantInfo?.totalRatingsString} </p>
+                </div>
 
+
+            </div>
+            <div className="menuInfo restContainer">
+
+                {menu.map((item) => (
+                    <div
+                        key={item.card.info.id}
+                        className="menuCard restaurantCard">
+                        <h5>
+                            {item.card.info.name}
+                        </h5>
+                        {/* <img
+                            src={CDN_URL + item.card.info.imageId}
+                            alt="menuImg"
+                        /> */}
+                        <h6>
+                            {item.card.info.price}
+                        </h6>
+                    </div>
+                ))}
+            </div>
 
         </div>
     )
