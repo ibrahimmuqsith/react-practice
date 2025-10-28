50ec5e1ac702ecf2e1971087db6baa257431878d
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-10-28 12:47:02 +0530

    fetched Menu from API int

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import {
     URL_IMG_MENU,
     MENU_ENDPOINT
@@ -13,17 +14,23 @@ const Menu = () => {
     const [menu, setMenu] = useState(null)
     const [restaurantInfo, setRestaurantInfo] = useState(null)
 
+    const { resId } = useParams()
+    // resId = 123456 for namasteDev menu data
+    // resId = 855614 for swiggy menu data
+
     useEffect(() => {
         fetchMenu()
     }, [])
 
     const fetchMenu = async () => {
         try {
-            // const res = await fetch(MENU_ENDPOINT);
-            // const data = await res.json();
-            // console.log("data", data)
-            setMenu(MENU_DATA)
-            setRestaurantInfo(SINGLE_RESTAURANT_DATA)
+            const res = await fetch(MENU_ENDPOINT + resId);
+            const data = await res.json();
+            const { data: { cards = [] } } = data
+            // setMenu(MENU_DATA)
+            // setRestaurantInfo(SINGLE_RESTAURANT_DATA)
+            setRestaurantInfo(cards[2]?.card?.card?.info)
+            setMenu(cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
         } catch (err) {
             console.log(err)
         }
@@ -51,30 +58,36 @@ const Menu = () => {
                 </div>
             </div>
             <div className="menuInfo restContainer">
-                {menu.map((item) => (
-                    <div
-                        key={item.card.info.id}
-                        className="menuCard restaurantCard flex-space-between"
-                    >
-                        <div className="">
-                            <h5>
-                                {item.card.info.name}
-                            </h5>
-                            <h6>
-                                Rs. {item.card.info.price / 100}
-                            </h6>
-                            <h6>
-                                serves {item.card.info.id}
-                            </h6>
-                        </div>
-                        <div className="">
-                            <img
-                                src={URL_IMG_MENU + item.card.info.imageId}
-                                alt="menuImg"
-                            />
-                        </div>
-                    </div>
-                ))}
+                {menu.map((item) => {
+                    return (
+                        item?.card?.card?.itemCards?.map(innerItem => {
+                            return (
+                                <div
+                                    key={innerItem.card.info.id}
+                                    className="menuCard restaurantCard flex-space-between"
+                                >
+                                    <div className="">
+                                        <h5>
+                                            {innerItem.card.info.name}
+                                        </h5>
+                                        <h6>
+                                            Rs. {innerItem.card.info.price / 100}
+                                        </h6>
+                                        <h6>
+                                            serves {innerItem.card.info.id}
+                                        </h6>
+                                    </div>
+                                    <div className="">
+                                        <img
+                                            src={URL_IMG_MENU + innerItem.card.info.imageId}
+                                            alt="menuImg"
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })
+                    )
+                })}
             </div>
         </div>
     )
